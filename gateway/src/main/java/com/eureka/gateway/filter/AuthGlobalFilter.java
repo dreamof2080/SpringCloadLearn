7efd3626d41d288a1033a02fb08a77f13ad1645d
jeffrey <906956368@qq.com>
2019-07-07 22:45:49 +0800

    网关服务

---
+++ gateway/src/main/java/com/eureka/gateway/filter/AuthGlobalFilter.java
@@ -0,0 +1,37 @@
+package com.eureka.gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthGlobalFilter implements GlobalFilter, Ordered {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("鉴权");
+        ServerHttpRequest request = exchange.getRequest();
+        logger.info(request.getURI().getPath());
+        logger.info(request.getURI().getPort()+"");
+        if (true) {
+            return chain.filter(exchange);
+        }
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        return response.setComplete();
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
