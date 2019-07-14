2a730cf377a63ee3c6e135681e59bcf1a5fa5dd4
jeffrey <906956368@qq.com>
2019-07-14 21:32:35 +0800

    和client1集成调试feign的使用

--- client2/src/main/java/com/eureka/client/controller/HelloWorldController.java
+++ client2/src/main/java/com/eureka/client/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.eureka.client.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,10 @@ public class HelloWorldController {
     public String sayHello1() {
         return "Hello World---2---";
     }
+
+    @RequestMapping(value = "/getName", method = RequestMethod.GET)
+    public String feign(){
+        return "this is client2";
+    }
+
 }
