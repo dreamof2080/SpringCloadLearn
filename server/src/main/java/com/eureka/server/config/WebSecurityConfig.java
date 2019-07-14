adbf24f2961c1537123e906e7fca7cbc21a61c35
jeffrey <906956368@qq.com>
2019-07-14 22:11:47 +0800

    打开security则默认开启csrf防御，这里需要过滤/eureka/**的请求不使用csrf，不然服务无法注册

--- server/src/main/java/com/eureka/server/config/WebSecurityConfig.java
+++ server/src/main/java/com/eureka/server/config/WebSecurityConfig.java
@@ -9,6 +9,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.csrf().disable();
+        http.csrf().ignoringAntMatchers("/eureka/**");
     }
 }
